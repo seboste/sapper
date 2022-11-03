2a7dbc21ad6587a76fc07343ca76dda45dc44d5e
Steger, Sebastian <Sebastian.Steger@gmail.com>
2022-11-03 22:06:08 +0100

    add unit test

---
+++ core/core_test.go
@@ -0,0 +1,18 @@
+package core
+
+import "testing"
+
+func TestNew(t *testing.T) {
+	c := Core{}
+	c.New()
+}
+
+func TestAdd(t *testing.T) {
+	c := Core{}
+	c.New()
+}
+
+func TestUpdate(t *testing.T) {
+	c := Core{}
+	c.New()
+}
