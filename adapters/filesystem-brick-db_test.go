5c885d354d3cb46a8dcd104548929bd30ed597e0
Steger, Sebastian <Sebastian.Steger@gmail.com>
2022-11-16 08:29:55 +0100

    Test_makeFilesystemBrick

--- adapters/filesystem-brick-db_test.go
+++ adapters/filesystem-brick-db_test.go
@@ -1,6 +1,9 @@
 package adapters
 
 import (
+	"log"
+	"os"
+	"path/filepath"
 	"reflect"
 	"testing"
 
@@ -48,6 +51,72 @@ func TestBrickKind_String(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := tt.bk.String(); got != tt.want {
 				t.Errorf("BrickKind.String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_makeFilesystemBrick(t *testing.T) {
+	tests := []struct {
+		name    string
+		yaml    string
+		files   []string
+		want    filesystemBrick
+		wantErr bool
+	}{
+		{name: "basic brick",
+			yaml: `id : test
+kind: extension
+description : My test brick
+version : 1.0.0
+parameters : 
+ - name : param1
+   default : default
+ - name : param2
+dependencies :
+ - dep1
+ - dep2`,
+			files: []string{"a", "b/c"},
+			want: filesystemBrick{
+				Id:           "test",
+				Description:  "My test brick",
+				Version:      "1.0.0",
+				Kind:         BrickKind(ports.Extension),
+				Parameters:   []ports.BrickParameters{{Name: "param1", Default: "default"}, {Name: "param2", Default: ""}},
+				Dependencies: []string{"dep1", "dep2"},
+				Files:        []string{"a", "b/c"},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			//1. prepare test
+			dir, _ := os.MkdirTemp("", "example")
+			defer os.RemoveAll(dir) // clean up
+			os.WriteFile(filepath.Join(dir, "manifest.yaml"), []byte(tt.yaml), 0666)
+
+			for _, file := range tt.files {
+				abspath := filepath.Join(dir, file)
+				filedir, _ := filepath.Split(abspath)
+
+				if err := os.MkdirAll(filedir, 0777); err != nil {
+					log.Fatalln(err)
+				}
+				if err := os.WriteFile(abspath, []byte{}, 0666); err != nil {
+					log.Fatalln(err)
+				}
+			}
+
+			//2. execute test
+			got, err := makeFilesystemBrick(dir)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("makeFilesystemBrick() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("makeFilesystemBrick() = %v, want %v", got, tt.want)
 			}
 		})
 	}
