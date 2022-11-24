d8b43ec710a460e9e0c5e68bc1c2728132830669
Steger, Sebastian <Sebastian.Steger@gmail.com>
2022-11-24 23:10:01 +0100

    added sections to bricks

---
+++ remote/handler/httplib/src/app/main.cpp
@@ -0,0 +1,20 @@
+//<<<SAPPER SECTION BEGIN APPEND MAIN-INCLUDES>>>
+#include <adapters/http-handler/http-handler.h>
+//<<<SAPPER SECTION END APPEND MAIN-INCLUDES>>>
+
+int main()
+{
+    ...
+
+//<<<SAPPER SECTION BEGIN APPEND MAIN-ADAPTER-INSTANTIATION>>>
+    HttpHandler handler(core);
+//<<<SAPPER SECTION END APPEND MAIN-ADAPTER-INSTANTIATION>>>
+
+    ...
+
+//<<<SAPPER SECTION BEGIN REPLACE MAIN-HANDLE>>>
+    handler.Handle();
+//<<<SAPPER SECTION END REPLACE MAIN-HANDLE>>>
+
+    return 0;
+}
