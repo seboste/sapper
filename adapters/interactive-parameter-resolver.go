3926d07699545612732b62b76dbd269f79c6a1c6
Steger, Sebastian <Sebastian.Steger@gmail.com>
2022-12-23 16:51:57 +0100

    interactive param resolver with default

--- adapters/interactive-parameter-resolver.go
+++ adapters/interactive-parameter-resolver.go
@@ -5,22 +5,41 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 
 	"github.com/seboste/sapper/ports"
 )
 
 type InteractiveParameterResolver struct {
+	DefaultResolver *ports.ParameterResolver
 }
 
-func resolve(rd io.Reader, name string) string {
+func resolve(rd io.Reader, wr io.Writer, name string, defaultValue string) string {
 	reader := bufio.NewReader(rd)
-	fmt.Printf("Enter value for parameter %s: ", name)
-	value, _ := reader.ReadString('\n')
+	value := ""
+	if defaultValue != "" {
+		fmt.Fprintf(wr, "Enter value for parameter %s or press enter for default %s: ", name, defaultValue)
+		value, _ = reader.ReadString('\n')
+		value = strings.TrimRight(value, "\n")
+		if value == "" {
+			value = defaultValue
+		}
+	} else {
+		for value == "" {
+			fmt.Fprintf(wr, "Enter value for parameter %s: ", name)
+			value, _ = reader.ReadString('\n')
+			value = strings.TrimRight(value, "\n")
+		}
+	}
 	return value
 }
 
 func (ipr InteractiveParameterResolver) Resolve(name string) string {
-	return resolve(os.Stdin, name)
+	defaultValue := ""
+	if ipr.DefaultResolver != nil {
+		defaultValue = (*ipr.DefaultResolver).Resolve(name)
+	}
+	return resolve(os.Stdin, os.Stdout, name, defaultValue)
 }
 
 var _ ports.ParameterResolver = InteractiveParameterResolver{}
--- adapters/interactive-parameter-resolver_test.go
+++ adapters/interactive-parameter-resolver_test.go
@@ -8,20 +8,29 @@ import (
 
 func Test_resolve(t *testing.T) {
 	type args struct {
-		rd   io.Reader
-		name string
+		rd           io.Reader
+		name         string
+		defaultValue string
 	}
 	tests := []struct {
-		name string
-		args args
-		want string
+		name       string
+		args       args
+		wantResult string
+		wantOutput string
 	}{
-		{name: "some parameter", args: args{rd: bytes.NewBufferString("value"), name: "param_1"}, want: "value"},
+		{name: "some parameter", args: args{rd: bytes.NewBufferString("value\n"), name: "param_1"}, wantResult: "value", wantOutput: "Enter value for parameter param_1: "},
+		{name: "some parameter with default", args: args{rd: bytes.NewBufferString("value\n"), name: "param_1", defaultValue: "defaultValue"}, wantResult: "value", wantOutput: "Enter value for parameter param_1 or press enter for default defaultValue: "},
+		{name: "pressing enter with default", args: args{rd: bytes.NewBufferString("\n"), name: "param_1", defaultValue: "defaultValue"}, wantResult: "defaultValue", wantOutput: "Enter value for parameter param_1 or press enter for default defaultValue: "},
+		{name: "pressing enter without default", args: args{rd: bytes.NewBufferString("\n\nvalue\n"), name: "param_1"}, wantResult: "value", wantOutput: "Enter value for parameter param_1: Enter value for parameter param_1: Enter value for parameter param_1: "},
 	}
 	for _, tt := range tests {
+		var b bytes.Buffer
 		t.Run(tt.name, func(t *testing.T) {
-			if got := resolve(tt.args.rd, tt.args.name); got != tt.want {
-				t.Errorf("resolve() = %v, want %v", got, tt.want)
+			if got := resolve(tt.args.rd, &b, tt.args.name, tt.args.defaultValue); got != tt.wantResult {
+				t.Errorf("resolve() = %v, want %v", got, tt.wantResult)
+			}
+			if b.String() != tt.wantOutput {
+				t.Errorf("resolve() output = %s, want %s", b.String(), tt.wantOutput)
 			}
 		})
 	}
